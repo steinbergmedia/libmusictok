b05492f3cf7dab99a84fcc808c35af0ba1b679d4
Andreas Papaeracleous <96296082+Darce-One@users.noreply.github.com>
2026-04-14 13:32:21 +0200

    update symusic version (#2)
    * update symusic version
    
    * update symusic version in base

--- external/symusic/add_symusic.cmake
+++ external/symusic/add_symusic.cmake
@@ -1,7 +1,7 @@
 FetchContent_Declare(
     symusic
     GIT_REPOSITORY https://github.com/Yikai-Liao/symusic
-    GIT_TAG 003e4f798d107833637effef8c730cdd05967467
+    GIT_TAG 937a74e336158673e3a1de995c3bbbb0cc3608ad
     PATCH_COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt <SOURCE_DIR>/CMakeLists.txt
 )
 FetchContent_MakeAvailable(symusic)
